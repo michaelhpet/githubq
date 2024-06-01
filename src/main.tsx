ed678d6722e43a5f63c5393564bac45eb72fe78e
Michael Peter <michaelhpet@gmail.com>
2024-06-01 20:43:32 +0100

    feat: setup project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "@/assets/styles/global.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import AppLayout from "./layouts";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: "/",
+          element: <AppLayout />,
+          children: [
+            { path: "/", element: <Home /> },
+            { path: "/:username", element: <Profile /> },
+          ],
+        },
+        { path: "*", element: <Navigate to="/" /> },
+      ])}
+    />
+  </React.StrictMode>
+);
