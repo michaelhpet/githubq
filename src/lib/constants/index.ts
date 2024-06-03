01037850100b74797ba84b45c34aef8d3d45d9a8
Michael Peter <michaelhpet@gmail.com>
2024-06-03 10:13:38 +0100

    feat: validate username and url

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,2 @@
+export const GITHUB_USERNAME_REGEX =
+  /^(?:(?:https?:\/\/)?(?:www\.)?github\.com\/)?(?<username>[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,37}[a-zA-Z0-9])?)\/?$/;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,19 @@
 import Button from "@/components/button";
+import { GITHUB_USERNAME_REGEX } from "@/lib/constants";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
 
   const submit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    navigate(`/${username}`);
+    const match = username.match(GITHUB_USERNAME_REGEX);
+    if (!match || !match.groups)
+      return setError("Please enter a valid username");
+    navigate(`/${match.groups.username}`);
   };
 
   return (
@@ -22,19 +27,24 @@ export default function Home() {
           activities, frequency of work, languages used, other important stuff.
         </p>
       </article>
-      <form
-        onSubmit={submit}
-        className="flex items-center justify-between gap-2 pr-4 border-[3px] border-stroke rounded-2xl bg-transparent outline-none has-[input:focus]:outline-[3px] has-[input:focus]:outline-accent has-[input:focus]:outline-offset-[3px]"
-      >
-        <input
-          placeholder="GitHub username or URL"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="w-full p-5 border-none outline-none bg-transparent text-3xl font-medium placeholder:text-dim"
-        />
-        <Button className="shrink-0 min-w-44 min-h-full text-2xl font-bold focus:outline-[3px] focus:outline-offset-[3px]">
-          Audit profile
-        </Button>
+      <form onSubmit={submit} className="flex flex-col gap-3">
+        <div className="flex items-center justify-between gap-2 pr-4 border-[3px] border-stroke rounded-2xl bg-transparent outline-none has-[input:focus]:outline-[3px] has-[input:focus]:outline-accent has-[input:focus]:outline-offset-[3px]">
+          <input
+            placeholder="GitHub username or URL"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+              setError("");
+            }}
+            className="w-full p-5 border-none outline-none bg-transparent text-3xl font-medium placeholder:text-dim"
+          />
+          <Button className="shrink-0 min-w-44 min-h-full text-2xl font-bold focus:outline-[3px] focus:outline-offset-[3px]">
+            Audit profile
+          </Button>
+        </div>
+        {Boolean(error) && (
+          <p className="text-sm text-red-500 font-medium">🚫&nbsp;{error}</p>
+        )}
       </form>
     </div>
   );
